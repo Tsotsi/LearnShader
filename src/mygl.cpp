c47e431a2c1a1c59033164d018729b1c819e49da
tsotsi <tsotsi@tsotsi.cn>
2022-07-06 13:47:09 +0800

    update

--- src/mygl.cpp
+++ src/mygl.cpp
@@ -8,7 +8,7 @@
 
 void error_callback(int error, const char* description)
 {
-    std::cerr << "Error: "<< description << std::endl;
+    std::cerr << "Error: " << error << ", " << description << std::endl;
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
