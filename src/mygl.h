c78ca9b0ca7a1d244420225cc87d6df1bc94f811
tsotsi <tsotsi@tsotsi.cn>
2022-07-07 19:35:34 +0800

    update

--- src/mygl.h
+++ src/mygl.h
@@ -7,13 +7,30 @@
 #ifdef __APPLE__
 //#define GL_SILENCE_DEPRECATION
 #endif
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
 
+#include <stdio.h>
+#include <bx/bx.h>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <GLFW/glfw3.h>
+#if BX_PLATFORM_LINUX
+#define GLFW_EXPOSE_NATIVE_X11
+#elif BX_PLATFORM_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#elif BX_PLATFORM_OSX
+#define GLFW_EXPOSE_NATIVE_COCOA
+#endif
+#include <GLFW/glfw3native.h>
+
+#include "glad/glad.h"
+#include "logo.h"
+
+static bool s_showStats = false;
 void error_callback(int error, const char* description);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void display(GLFWwindow* window, double currentTime);
 
-int appRun();
+int app();
+int appWithBgfx();
 #endif //LEARNSHADE_GL_H
--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,13 @@
 add_rules("mode.debug", "mode.release")
 
 
-add_requires("glfw ^3.3.4", "glad")
+add_requires("glfw ^3.3.4", "glad", "bgfx")
 
 target("LearnShade")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_packages("glfw", "glad")
+    add_packages("glfw", "glad", "bgfx")
+    set_languages("c17", "cxx14")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
